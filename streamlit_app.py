bb600c04c82d2f43567f4d20d768d0f88be3841e
Annonic2023 <131668880+Annonic2023@users.noreply.github.com>
2023-09-26 18:37:54 +0200

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-#import plotly.express as px
-#import plotly.graph_objects as go
 from urllib.request import urlopen
 import json
 from copy import deepcopy
@@ -62,58 +60,7 @@ ax.set_title("Engine Size vs. Highway Fuel Mileage")
 ax.set_xlabel('Displacement (Liters)')
 ax.set_ylabel('MPG')
 st.pyplot(m_fig)
-# In Plotly
-#p_fig = px.scatter(reduced_df, x='displ', y='hwy', opacity=0.5,
-#                   range_x=[1, 8], range_y=[10, 50],
-#                   width=750, height=600,
-#                   labels={"displ": "Displacement (Liters)",
-#                           "hwy": "MPG"},
-#                   title="Engine Size vs. Highway Fuel Mileage")
-#p_fig.update_layout(title_font_size=22)
-
-#if show_means == "Yes":
-#    p_fig.add_trace(go.Scatter(x=means['displ'], y=means['hwy'],
-#                               mode="markers"))
-#    p_fig.update_layout(showlegend=False)
-
-# Select which plot to show
-#if plot_type == "Matplotlib":
-#    st.pyplot(m_fig)
-#else:
-#    st.plotly_chart(p_fig)
 
 # We can write stuff
 url = "https://archive.ics.uci.edu/ml/datasets/auto+mpg"
 st.write("Data Source:", url)
-# "This works too:", url
-
-# Another header
-#st.header("Maps")
-
-# Sample Streamlit Map
-#st.subheader("Streamlit Map")
-#ds_geo = px.data.carshare()
-#ds_geo['lat'] = ds_geo['centroid_lat']
-#ds_geo['lon'] = ds_geo['centroid_lon']
-#st.map(ds_geo)
-
-# Sample Choropleth mapbox using Plotly GO
-#st.subheader("Plotly Map")
-
-#with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-#    counties = json.load(response)
-#df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
-#                 dtype={"fips": str})
-
-#plotly_map = go.Figure(go.Choroplethmapbox(geojson=counties,
-#                                           locations=df.fips,
-#                                           z=df.unemp,
-#                                           colorscale="Viridis",
-#                                           zmin=0, zmax=12,
-#                                           marker={"opacity": 0.5, "line_width": 0}))
-#plotly_map.update_layout(mapbox_style="carto-positron",
-#                         mapbox_zoom=3,
-#                         mapbox_center={"lat": 37.0902, "lon": -95.7129},
-#                         margin={"r": 0, "t": 0, "l": 0, "b": 0})
-
-#st.plotly_chart(plotly_map)
